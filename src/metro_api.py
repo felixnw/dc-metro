768af9385f6bbf961b96a61dd6df7452e52ccb19
GJT-34 <125122551+GJT-34@users.noreply.github.com>
2023-03-12 23:56:37 -0400

    Update metro_api.py

--- src/metro_api.py
+++ src/metro_api.py
@@ -1,65 +1,191 @@
-import board
-from adafruit_matrixportal.network import Network
-
+import time
 from config import config
-from secrets import secrets
-
-# Keeping a global reference for this
-_network = Network(status_neopixel=board.NEOPIXEL)
 
 class MetroApiOnFireException(Exception):
     pass
 
 class MetroApi:
-    def fetch_train_predictions(station_code: str, group: str) -> [dict]:
-        return MetroApi._fetch_train_predictions(station_code, group, retry_attempt=0)
+    def __init__(self):
+        pass
 
-    def _fetch_train_predictions(station_code: str, group: str, retry_attempt: int) -> [dict]:
+    def fetch_train_predictions(self, wifi, station_code: str) -> [dict]:
+        return self._fetch_train_predictions(wifi, station_code, retry_attempt=0)
+
+    def _fetch_train_predictions(self, wifi, station_code: str, retry_attempt: 
+                                 int) -> [dict]:
         try:
+            print("Fetching...")
+
+            # Counter potential null values in API response, as python doesn't recognize null
+            null = None
+
+            # Construct a URL that will fetch data for our desired station
             api_url = config['metro_api_url'] + station_code
-            train_data = _network.fetch(api_url, headers={
-                'api_key': config['metro_api_key']
-            }).json()
 
-            print('Received response from WMATA api...')
+            # Fetch data for our desired station from the URL
+            response = wifi.get(api_url, headers={'api_key': config['metro_api_key']},
+                                timeout=1).json()
 
-            trains = filter(lambda t: t['Group'] == group, train_data['Trains'])
+            # Filter train data so only objects for our selected group are in the list.
+            trains = list(filter(lambda t: t['Group'] == config['train_group'], 
+                          response['Trains']))
+            print("Received response from API. The filtered result:")
+            print(trains)
+            
+            # Switch to showing result for both groups if there are no trains with assigned lines 
+            # in the selected group AND the user wants result from both lines in this instance. 
+            # This can be useful when single-tracking occurs and one group switches to the other.
+            # 'No psngr' trains with no assigned lines are ignored in figuring the group to show.            
+            trains_test = list(filter(lambda t: t['Line'] in ['BL', 'GR', 'OR', 'RD', 'SV', 'YL'], 
+                                      trains))
+            if not trains_test and config['show_all_groups_if_nothing_else']:
+                print("No trains with an assigned line in this group.")
+                trains_alt = list(response['Trains'])
+                trains_alt = trains_alt[:config['num_trains']]
+                print("The re-filtered result, expanded to include the other one or two groups:")
+                print(trains_alt)
+                trains_test = list(filter(lambda t: t['Line'] in ['BL', 'GR', 'OR', 'RD', 'SV',
+                                   'YL'], trains_alt))
+                if not trains_test:
+                    print("No trains with an assigned line in any group, so using the initial \
+                          result as the basis for display.")
+                else:
+                    trains = list(response['Trains'])
+                    print("Using the re-filtered result as the basis for display.")
+                            
+            # Convert train objects list to a custom list with data needed for display
+            trains = [self._normalize_train_response(t) for t in trains][:config['num_trains']]
+            print("Normalized train response:")
+            for train in trains:
+                print(train)
 
-            normalized_results = list(map(MetroApi._normalize_train_response, trains))
+            return trains
 
-            return normalized_results
-        except RuntimeError:
+        except Exception as e:
+            print(e)
             if retry_attempt < config['metro_api_retries']:
-                print('Failed to connect to WMATA API. Reattempting...')
+                print("Failed to connect to API. Retrying...")
                 # Recursion for retry logic because I don't care about your stack
-                return MetroApi._fetch_train_predictions(station_code, group, retry_attempt + 1)
+                time.sleep(config['refresh_interval'])
+                return self._fetch_train_predictions(wifi, station_code, 
+                                                     retry_attempt + 1)
             else:
                 raise MetroApiOnFireException()
-    
-    def _normalize_train_response(train: dict) -> dict:
-        line = train['Line']
-        destination = train['Destination']
-        arrival = train['Min']
 
-        if destination == 'No Passenger' or destination == 'NoPssenger' or destination == 'ssenger':
-            destination = 'No Psngr'
+    # Take a JSON object for single train and return only the data needed for display
+    def _normalize_train_response(self, train: dict) -> dict:
+        line = train['Line']
+        line = self._get_line_color(line)
+
+        car = train['Car']
+        car = self._get_corrected_car_value(car)
+
+        destination = train['Destination']
+        destination = self._get_corrected_dest_case(destination)
+        destination = self._get_corrected_dest_value(destination)
+
+        min = train['Min']
+        min = self._get_corrected_min_value(min)
 
         return {
-            'line_color': MetroApi._get_line_color(line),
+            'line': line,
+            'car': car,
             'destination': destination,
-            'arrival': arrival
+            'min': min
         }
-    
-    def _get_line_color(line: str) -> int:
-        if line == 'RD':
-            return 0xFF0000
-        elif line == 'OR':
-            return 0xFF5500
-        elif line == 'YL':
-            return 0xFFFF00
+
+    # Convert two-letter 'line" value into equivalent integer
+    def _get_line_color(self, line: str) -> int:
+        if line == 'BL':
+            return 0x0000FF
         elif line == 'GR':
             return 0x00FF00
-        elif line == 'BL':
-            return 0x0000FF
-        else:
+        elif line == 'OR':
+            return 0xFF5500
+        elif line == 'RD':
+            return 0xFF0000
+        elif line == 'SV':
             return 0xAAAAAA
+        elif line == 'YL':
+            return 0xFFFF00
+        else:
+            return 0x000000
+
+    # Respond to empty 'car' value
+    def _get_corrected_car_value(self, car: str) -> str:
+        if car is None:
+            car = '-'
+        return car
+    
+    # Convert 'Destination' WMATA API result from all caps to title case
+    def _get_corrected_dest_case(self, destination: str) -> str:
+        # The WMATA API sometimes reports 'Destination' in all caps. Changing from all caps to 
+        # titlecase takes extra work because .title() does not work in CircuitPython. 
+        all_caps = 0
+        dest_replace = ""
+        if len(destination) > 0 and destination.isupper():
+            destination = destination.lower()
+            for word in destination.split():
+                dest_replace += word[0].upper() + word[1:] + " "    
+            dest_replace = dest_replace.rstrip()
+            destination = dest_replace
+        return destination
+    
+    # Correct for problems in 'Destination' WMATA API result
+    def _get_corrected_dest_value(self, destination: str) -> str:
+        # The 'Destination' reported by the WMATA API sometimes needs correcting. For instance, 
+        # instead of 'NewCrltn', the API may use 'N Carrollton', which is too long to display 
+        # properly. To address this, a dictionary is used to correct for known and potential 
+        # problems in API-reported data. Also included in the dictionary any extra-long 
+        # destinations that we don't want cropped, such as the 9-char 'Gallry Pl' (which displays
+        # OK despite its length when using a variable-width font).
+        dest_corrections = {
+            'No Passenger': 'No Psngr',
+            'NoPssenger': 'No Psngr',
+            'ssenger': 'No Psngr',
+            'Branch': 'Brnch Av',
+            'Brnch Ave': 'Brnch Av',
+            'Downtown': 'Largo',
+            'DT Largo': 'Largo',
+            'Farragut N': 'Frgut N.',
+            'Ft. Tottn': 'Ft.Tottn',
+            'Totten': 'Ft.Tottn',
+            'Franconia': 'Frnconia',
+            'Chinatown': 'Gallry Pl',
+            'Gallery': 'Gallry Pl',
+            'Gallry Pl': 'Gallry Pl',
+            'GallryPl': 'Gallry Pl',
+            'Greenbelt': 'Grnbelt',
+            'Grosvnor': 'Grsvnor',
+            'Huntington': 'Hntingtn',
+            'Vernon': 'Mt. Vern',
+            'Carroll': 'NewCrltn',
+            'Noma': 'NoMa',
+            'Shady': 'Shady Gr',
+            'Silver': 'SilvrSpr',
+            'Silvrspr': 'SilvrSpr',
+            'U St': 'U St',
+            'Fairfax': 'Vienna',
+            'Dulles': 'Dulles',
+            'W Falls': 'W Fls Ch',
+            'West Falls': 'W Fls Ch'
+        }
+        dest_replace = ""
+        for key in dest_corrections.keys():
+            if key in destination:
+                dest_replace = dest_corrections[key]
+        if dest_replace:
+            destination = dest_replace
+        else:
+            destination = destination[:config['dest_max_characters']]
+        return destination
+    
+    # Reduce three-character 'min' values to two characters, to save space on the train board
+    def _get_corrected_min_value(self, min: str) -> str:
+        if min == 'ARR':
+            min = 'AR'
+        if min == 'BRD':
+            min = 'BD'
+        if min == '---' or min == '':
+            min = 'DL'
+        return min
